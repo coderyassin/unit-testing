2a8decc9dbe924e195775be1df5a7f2fed8756d6
Yassin MELLOUKI <yassin.mellouki.j@gmail.com>
2025-02-02 15:05:55 +0100

    first commit

---
+++ src/main/java/org/yascode/unit_testing/service/TodoService.java
@@ -0,0 +1,7 @@
+package org.yascode.unit_testing.service;
+
+import java.util.List;
+
+public interface TodoService {
+    List<String> retrieveTodos(String user);
+}
---
+++ src/main/java/org/yascode/unit_testing/workflow/TodoWorkflow.java
@@ -0,0 +1,7 @@
+package org.yascode.unit_testing.workflow;
+
+import java.util.List;
+
+public interface TodoWorkflow {
+    List<String> retrieveTodosRelatedToJava(String user);
+}
